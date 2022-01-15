6bc8e00e5f13ea00835614a48c06a65439a3c735
Abhijit Jha <abhijeetmitian1@gmail.com>
2022-01-15 19:18:00 +0530

    i have changed some settings,py added a procfile and requirement.txt
    i have changed some settings,py added a procfile and requirement.txt

--- bpf/bpf/settings.py
+++ bpf/bpf/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-no06zfo21fb1rqcxjmwba_qemkg6nt-$st8*yxs3fl3z7w+6ec'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','best-product-findr.herokuapp.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
